ad2ba977c28e81aa803066e66c90c3259ee3d506
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-04 18:47:03 -0300

    creating router post signup and validations

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -7,3 +7,29 @@ import userModel from "../models/user.model.js";
 import tokenMiddleware from "../middlewares/token.middleware.js";
 
 const router = express.Router();
+
+router.post(
+  "/signup",
+  body("username")
+    .exists().withMessage("username is required")
+    .isLength({ min: 8 }).withMessage("username minimum 8 characters")
+    .custom(async value => {
+      const user = await userModel.findOne({ username: value });
+      if (user) return Promise.reject("username already used");
+    }),
+  body("password")
+    .exists().withMessage("password is required")
+    .isLength({ min: 8 }).withMessage("password minimum 8 characters"),
+  body("confirmPassword")
+    .exists().withMessage("confirmPassword is required")
+    .isLength({ min: 8 }).withMessage("confirmPassword minimum 8 characters")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) throw new Error("confirmPassword not match");
+      return true;
+    }),
+  body("displayName")
+    .exists().withMessage("displayName is required")
+    .isLength({ min: 8 }).withMessage("displayName minimum 8 characters"),
+  requestHandler.validate,
+  userController.signup
+);
