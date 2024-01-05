fc1a55f466d3dd442dee10a1e4a97e0077100249
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-05 18:50:38 -0300

    creating router post / review.route.js

--- server/src/routes/review.route.js
+++ server/src/routes/review.route.js
@@ -12,3 +12,22 @@ router.get(
     reviewController.getReviewOfUser
 )
 
+router.post(
+    '/',
+    tokenMiddleware.auth,
+    body("mediaId")
+    .exists().withMessage("mediaId is required")
+    .isLength({ min: 1 }).withMessage("mediaId can not be empty"),
+    body("content")
+    .exists().withMessage("content is required")
+    .isLength({ min: 1 }).withMessage("content can not be empty"),
+    body("mediaType")
+    .exists().withMessage("mediaType is required")
+    .custom(type => ["movie", "tv"].includes(type)).withMessage("mediaType invalid"),
+    body("mediaTitle")
+        .exists().withMessage("mediaTitle is required"),
+    body("mediaPoster")
+        .exists().withMessage("mediaPoster is required"),
+    requestHandler.validate,
+    reviewController.create
+);
