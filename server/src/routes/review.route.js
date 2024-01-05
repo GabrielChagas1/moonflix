5a643cc9c31e33ffacfda1ebac7bffc805114b06
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-05 18:50:23 -0300

    creating router get / review.route.js

--- server/src/routes/review.route.js
+++ server/src/routes/review.route.js
@@ -4,3 +4,11 @@ import reviewController from "../controllers/review.controller.js"
 import tokenMiddleware from "../middlewares/token.middleware.js"
 import requestHandler from "../handlers/request.handler.js"
 
+const router = express.Router({ mergeParams: true })
+
+router.get(
+    '/',
+    tokenMiddleware.auth,
+    reviewController.getReviewOfUser
+)
+
