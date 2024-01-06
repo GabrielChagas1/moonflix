01d29bda3d6871b055434a2b8db878a6dae6fac2
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-06 18:54:34 -0300

    import express and routes

---
+++ server/src/routes/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+import userRoute from "./user.route.js";
+import mediaRoute from "./media.route.js";
+import personRoute from "./person.route.js";
+import reviewRoute from "./review.route.js";
