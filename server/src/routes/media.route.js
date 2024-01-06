9953a60c96f997b027faa31f3dd9eb8800be822b
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-06 18:53:05 -0300

    import router genres media.route.js

--- server/src/routes/media.route.js
+++ server/src/routes/media.route.js
@@ -4,3 +4,4 @@ import mediaController from "../controllers/media.controller.js"
 const router = express.Router({mergeParams: true})
 
 router.get('/search', mediaController.search)
+router.get('/genres', mediaController.getGenres)
