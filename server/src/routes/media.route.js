1a08d21b965b0f04e651fe53e7972a6014b850f6
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-06 18:52:55 -0300

    import router search media.route.js

--- server/src/routes/media.route.js
+++ server/src/routes/media.route.js
@@ -2,3 +2,5 @@ import express from "express"
 import mediaController from "../controllers/media.controller.js"
 
 const router = express.Router({mergeParams: true})
+
+router.get('/search', mediaController.search)
