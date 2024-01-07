7db25e6c57382c03e296115f923bd9b639a0077d
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-07 18:54:54 -0300

    creating router using express.Router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,3 +3,5 @@ import userRoute from "./user.route.js";
 import mediaRoute from "./media.route.js";
 import personRoute from "./person.route.js";
 import reviewRoute from "./review.route.js";
+
+const router = express.Router();
