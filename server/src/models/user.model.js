b0bc769fc5f1b72de3dd13713f725639aeaf4c3a
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-07 18:57:04 -0300

    creating userSchema

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,3 +1,25 @@
 import mongoose, { model } from "mongoose"
 import modelOptions from "./model.options.js"
 import crypto from "crypto"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    salt: {
+        type: String,
+        required: true,
+        select: false
+    }
+}, modelOptions)
