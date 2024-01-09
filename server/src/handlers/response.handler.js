5b58015426ac2ac2e4a583be71677d7ea435945c
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-09 19:01:56 -0300

    creating error

--- server/src/handlers/response.handler.js
+++ server/src/handlers/response.handler.js
@@ -1,1 +1,7 @@
 const responseWithData = (res, statusCode, data) => res.status(statusCode).json(data)
+
+const error = (res) => responseWithData(res, 500, {
+    status: 500,
+    message: "Oops! Something wrong!"
+})
+
