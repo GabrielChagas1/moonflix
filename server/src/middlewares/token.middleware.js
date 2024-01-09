d6443c8e6caf3c5214ee1ae8366713eecf391194
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-09 19:01:15 -0300

    creating auth token.middleware.js

--- server/src/middlewares/token.middleware.js
+++ server/src/middlewares/token.middleware.js
@@ -20,3 +20,15 @@ const tokenDecode = (req) => {
     }
 }
  
+const auth = async (req, res, next) => {
+    const tokenDecode = tokenDecode(req)
+
+    if(!tokenDecode) return responseHandler.unauthorize(res)
+
+    const user = await userModel.findById(tokenDecode.data)
+
+    if(!user) return responseHandler.unauthorize(res)
+
+    req.user = user
+    next()
+}
