600586e3f263e7d2362f94bb4f5357be1db29f68
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-09 19:00:57 -0300

    creating tokenDecode

--- server/src/middlewares/token.middleware.js
+++ server/src/middlewares/token.middleware.js
@@ -1,3 +1,22 @@
 import jsonwebtoken from "jsonwebtoken"
 import responseHandler from "../handlers/response.handler.js"
 import userModel from "../models/user.model.js"
+
+const tokenDecode = (req) => {
+    try {
+        const bearerHeader = req.headers["authorization"]
+
+        if(bearerHeader){ 
+            const token = bearerHeader.split(" ")[1]
+
+            return jsonwebtoken.verify(
+                token, 
+                process.env.TOKEN_SECRET
+            )
+        }
+        return false
+    } catch (error) {
+        return false
+    }
+}
+ 
