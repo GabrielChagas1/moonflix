aeb68838c12d9109359feaba60abb3043356736c
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-10 19:05:10 -0300

    creating getInfo user.controller.js

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -84,3 +84,17 @@ const updatePassword = async (req, res) => {
         responseHandler.error(res)
     }
 }
+
+const getInfo = async (req, res) => {
+    try {
+        const user = await userModel.findById(req.user.id)
+
+        if(!user) return responseHandler.notfound(res)
+
+        responseHandler.ok(res, user)
+
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
