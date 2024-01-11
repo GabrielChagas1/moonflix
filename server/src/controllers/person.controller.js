31d6e321cf43f14af73ff9dd9b3fc2b2fb4e2c39
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-11 19:07:23 -0300

    creating personDetail person.controller.js

--- server/src/controllers/person.controller.js
+++ server/src/controllers/person.controller.js
@@ -1,2 +1,16 @@
 import responseHandler from "../handlers/response.handler.js";
 import tmdbApi from "../tmdb/tmdb.api.js";
+
+const personDetail = async (req, res) => {
+    try {
+        const { personId } = req.params
+
+        const person = await tmdbApi.personDetail({personId})
+
+        responseHandler.ok(res, person)
+
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
