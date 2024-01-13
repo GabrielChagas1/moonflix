86e1b7e1ae5407845373422d528877e83fbd6f0c
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-13 19:12:40 -0300

    creating removeFavorite

--- server/src/controllers/favorite.controller.js
+++ server/src/controllers/favorite.controller.js
@@ -25,3 +25,23 @@ const addFavorite = async (req, res) => {
 }
 
 
+const removeFavorite = async (req, res) => {
+    try {
+        const { favoriteId } = req.params;
+
+        const favorite = await favoriteModel.findOne({
+            user: req.user.id,
+            _id: favoriteId
+        })
+
+        if(!favorite) return responseHandler.notfound(res)
+
+        await favorite.remove()
+
+        responseHandler.ok(res)
+
+    } catch {
+        responseHandler.error(res)
+    }
+}
+
