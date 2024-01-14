72ed77ac3dbb9c077d18c370c13065a4e8f7f856
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-14 19:14:02 -0300

    creating routes

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -18,3 +18,57 @@ export const routesGen = {
   reviewList: "/reviews",
   passwordUpdate: "password-update"
 };
+
+const routes = [
+  {
+    index: true,
+    element: <HomePage />,
+    state: "home"
+  },
+  {
+    path: "/person/:personId",
+    element: <PersonDetail />,
+    state: "person.detail"
+  },
+  {
+    path: "/search",
+    element: <MediaSearch />,
+    state: "search"
+  },
+  {
+    path: "/password-update",
+    element: (
+      <ProtectedPage>
+        <PasswordUpdate />
+      </ProtectedPage>
+    ),
+    state: "password.update"
+  },
+  {
+    path: "/favorites",
+    element: (
+      <ProtectedPage>
+        <FavoriteList />
+      </ProtectedPage>
+    ),
+    state: "favorites"
+  },
+  {
+    path: "/reviews",
+    element: (
+      <ProtectedPage>
+        <ReviewList />
+      </ProtectedPage>
+    ),
+    state: "reviews"
+  },
+  {
+    path: "/:mediaType",
+    element: <MediaList />
+  },
+  {
+    path: "/:mediaType/:mediaId",
+    element: <MediaDetail />
+  }
+];
+
