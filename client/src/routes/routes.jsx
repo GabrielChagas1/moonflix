6b48a2739db2be7b7a38635b8bd6b66c275b5289
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-14 19:13:50 -0300

    creating routesGen

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -7,3 +7,14 @@ import MediaSearch from "../pages/MediaSearch";
 import PasswordUpdate from "../pages/PasswordUpdate";
 import ReviewList from "../pages/ReviewList";
 import ProtectedPage from "../components/common/ProtectedPage";
+
+export const routesGen = {
+  home: "/",
+  mediaList: (type) => `/${type}`,
+  mediaDetail: (type, id) => `/${type}/${id}`,
+  mediaSearch: "/search",
+  person: (id) => `/person/${id}`,
+  favoriteList: "/favorites",
+  reviewList: "/reviews",
+  passwordUpdate: "password-update"
+};
