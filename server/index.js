620433c409dd706362bd8016830618a963f37228
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-14 19:14:32 -0300

    import express and others

---
+++ server/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+import routes from "./src/routes/index.js";
