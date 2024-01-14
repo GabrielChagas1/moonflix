c601a53a80670994af3c967674a07070027c63b3
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-14 19:14:54 -0300

    creating app.use

--- server/index.js
+++ server/index.js
@@ -7,3 +7,8 @@ import "dotenv/config";
 import routes from "./src/routes/index.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
