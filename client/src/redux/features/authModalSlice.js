64c85fa4cd305baf2b7adf47c009018d13d6cff8
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-17 19:42:49 -0300

    creating authModalSlice

--- client/src/redux/features/authModalSlice.js
+++ client/src/redux/features/authModalSlice.js
@@ -1,1 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+export const authModalSlice = createSlice({
+  name: "AuthModal",
+  initialState: {
+    authModalOpen: false
+  },
+  reducers: {
+    setAuthModalOpen: (state, action) => {
+      state.authModalOpen = action.payload;
+    }
+  }
+});
