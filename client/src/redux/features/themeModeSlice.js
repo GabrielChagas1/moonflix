d810dbe61058a8733a1a65b45480134f46fc55c2
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-17 19:40:57 -0300

    creating const themeModeSlice

--- client/src/redux/features/themeModeSlice.js
+++ client/src/redux/features/themeModeSlice.js
@@ -1,1 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+export const themeModeSlice = createSlice({
+  name: "ThemeMode",
+  initialState: {
+    themeMode: "dark"
+  },
+  reducers: {
+    setThemeMode: (state, action) => {
+      state.themeMode = action.payload;
+    }
+  }
+});
