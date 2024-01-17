bfbac70496dfb46069ec2b7e41c08a4c2f1cceb3
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-17 19:42:04 -0300

    creating globalLoadingSlice

--- client/src/redux/features/globalLoadingSlice.js
+++ client/src/redux/features/globalLoadingSlice.js
@@ -1,1 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+export const globalLoadingSlice = createSlice({
+  name: "AuthModal",
+  initialState: {
+    globalLoading: false
+  },
+  reducers: {
+    setGlobalLoading: (state, action) => {
+      state.globalLoading = action.payload;
+    }
+  }
+});
