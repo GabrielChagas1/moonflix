f762817321d909056e917aa633372e1894f3fedb
Gabriel Chagas <gabrielserq@outlook.com>
2024-01-18 19:43:25 -0300

    creating appStateSlice

--- client/src/redux/features/appStateSlice.js
+++ client/src/redux/features/appStateSlice.js
@@ -1,1 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+export const appStateSlice = createSlice({
+  name: "AppState",
+  initialState: {
+    appState: ""
+  },
+  reducers: {
+    setAppState: (state, action) => {
+      state.appState = action.payload;
+    }
+  }
+});
+
